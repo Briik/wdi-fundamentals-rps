0cf0cb27c0d6cce9e68df5fb4a467dbca400a82f
Bryce Lively <BryceLively@Bryces-MacBook-Pro.local>
2015-09-28 10:54:06 -0400

    completed f playToFive

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,8 +25,7 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return var move;
-    move || getInput;
+    return move || getInput;
 }
 
 function getComputerMove(move) {
@@ -63,8 +62,25 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    // This function should continue to play Rock Paper Scissors until either the
+    // player or the computer has won five times.
+    // After each 'round', display some text in the console indicating who played
+    // what, who won, and what the current scoreboard looks like.
+    // For example,
+    //  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+    //  console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    for (var playCount = 1; ((playerWins < 5) || (computerWins < 5)); console.log('Scoreboard... Player: ' + playerWins + ' Computer: ' + computerWins + '\n'), playCount += 1) {
+        getPlayerMove();
+        getComputerMove();
+        getWinner();
+        if (getWinner != "tie") {
+        } else if (getWinner = "player") { 
+            playerWins + 1;
+        } else if (getWinner = "computer") { 
+            computerWins += 1;
+        }
+        console.log('Player chose ' + playerMove + '. Computer chose ' + computerMove + '. The' + winner + 'wins round' + playCount + '.' + '\n');
+    }
+    return [playerWins, computerWins, playCount];
 }
 
